885274d3067d6ea4a69514441a7554a44e677754
L1MJH <lolflgc@icloud.com>
2023-05-26 02:17:42 +0800

    Added login.js, register.js, layout.js

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Login() {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      
+      <h1>Register</h1>
+    </div>
+  );
+}
