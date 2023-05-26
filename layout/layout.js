0061e886432f4f49b93b8defa76dc270b9bdf952
L1MJH <lolflgc@icloud.com>
2023-05-26 11:20:47 +0800

    Added animations for login page

--- layout/layout.js
+++ layout/layout.js
@@ -6,6 +6,8 @@ export default function Layout({ children }) {
       <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
         <div className={styles.imgStyle}>
           <div className={styles.cartoonImg}></div>
+          <div className={styles.cloud_one}></div>
+          <div className={styles.cloud_two}></div>
         </div>
         <div className="right-flex flex-col justify-evenly bg-gray-500">
           <div className="text-center py-10">{children}</div>
