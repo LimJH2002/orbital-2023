4925d662d94805d40be948f2c01ee9d7661ca197
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-05-26 23:58:48 +0800

    init firebase
    npm install firebase

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,17 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ4iQxM4SiVncO2W49X2WZkrAoizeR-pw",
+  authDomain: "finforce-orbital.firebaseapp.com",
+  projectId: "finforce-orbital",
+  storageBucket: "finforce-orbital.appspot.com",
+  messagingSenderId: "904834984824",
+  appId: "1:904834984824:web:726eb0d563b9c4cc0d368d",
+  measurementId: "G-TZ5Q6M846M"
+};
+
 export default function Home() {
   return (
     <>
