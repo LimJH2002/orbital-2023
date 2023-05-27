be0355db9497cf8744e99759f7272e0d6a7e797b
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-05-27 19:41:38 +0800

    added Google Client ID and Secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,1 +1,12 @@
 import NextAuth from 'next-auth';
+import GoogleProvider from "next-auth/providers/google"
+
+export default NextAuth({
+    providers: [
+        // Google
+        GoogleProvider({
+            clienId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ]
+})
