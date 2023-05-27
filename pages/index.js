cb6a7d890cab75ebb1b62ba94691bb590174b6ec
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-05-28 02:17:54 +0800

    Create firebaseApp.js

--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,6 @@ import { getSession, useSession, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBL_7OqbwBKtqR5FJHFj11Ay5ENCyKsPXE",
-  authDomain: "finforce-o.firebaseapp.com",
-  projectId: "finforce-o",
-  storageBucket: "finforce-o.appspot.com",
-  messagingSenderId: "487993412863",
-  appId: "1:487993412863:web:bfb24ea755ea9913f490ca",
-  measurementId: "G-JHD0Y2CSY4",
-};
-
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 export default function Home() {
   const { data: session } = useSession();
 
