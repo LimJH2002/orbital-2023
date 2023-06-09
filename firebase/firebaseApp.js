b593ed36c59ff73a226f738429333f2b603f6064
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-06-10 02:05:23 +0800

    init firebase functions

--- firebase/firebaseApp.js
+++ firebase/firebaseApp.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBL_7OqbwBKtqR5FJHFj11Ay5ENCyKsPXE",
-    authDomain: "finforce-o.firebaseapp.com",
-    projectId: "finforce-o",
-    storageBucket: "finforce-o.appspot.com",
-    messagingSenderId: "487993412863",
-    appId: "1:487993412863:web:bfb24ea755ea9913f490ca",
-    measurementId: "G-JHD0Y2CSY4",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   };
   
   // Initialize Firebase
