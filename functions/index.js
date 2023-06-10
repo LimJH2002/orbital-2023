6339e7bf9aaba9d0db22f42fa0c5ca40704c5064
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-06-10 18:16:24 +0800

    updated functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,16 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+import * as functions from "firebase-functions";
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+export const createUserDocument = functions.auth.user().onCreate((user) => {
+    db.collection("users")
+        .doc(user.uid)
+        .set(JSON.parse(JSON.stringify(user)));
+});
