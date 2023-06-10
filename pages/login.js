95ee36ffbfc9358993204c9348387b1f15ba6154
L1MJH <lolflgc@icloud.com>
2023-06-10 15:16:11 +0800

    Revert "Renamed files to kebab-case and organise layouts"
    This reverts commit 66dc70419f93800afabbc69b5d06dd6c7253bb80.

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "../layouts/login-layout";
+import Layout from "../layout/layout";
 import Link from "next/link";
 import styles from "../styles/Form.module.css";
 import Image from "next/image";
