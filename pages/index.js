b34b1e123c14af3f32ca22e559f52d6f78871917
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-06-12 15:17:27 +0800

    update to use useEffect

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,17 @@ import Layout from "@/components/layout";
 import { useAuth } from "@/context/AuthContext";
 import Overlap from "@/components/overlap-banner";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
   const { logout, currentUser } = useAuth();
   const router = useRouter();
-  if (!currentUser) {
-    router.push("/login")
-  }
-
+  useEffect(() => {
+    if(!currentUser) {
+      router.push("/login");  
+    }
+  })
+  console.log(currentUser);
   return (
     <>
       <Head>
