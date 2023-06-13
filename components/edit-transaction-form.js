bba362e7083a2701c51e756a02dfb9c58cc55a6a
L1MJH <lolflgc@icloud.com>
2023-06-13 13:10:26 +0800

    Added delete button for edit transaction

--- components/edit-transaction-form.js
+++ components/edit-transaction-form.js
@@ -137,6 +137,12 @@ export default function EditTransactionForm({ transaction, func }) {
             Cancel
           </button>
           <button
+            type="button"
+            className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+          >
+            Delete
+          </button>
+          <button
             type="submit"
             className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
