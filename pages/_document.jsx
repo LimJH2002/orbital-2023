1232fb2aaa90bf0193fd70295954239209741b0b
L1MJH <lolflgc@icloud.com>
2023-06-15 21:24:20 +0800

    Added Personal Information section

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='bg-gray-900'>
       <Head />
-      <body>
+      <body className='h-full'>
         <Main />
         <NextScript />
       </body>
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,10 +1,119 @@
-import React from "react";
-import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Image from "next/image";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Loading from "./loading";
 
-const Profile = () => {
-  const router = useRouter();
-  router.push("/comingSoon");
-  return <div></div>;
-};
+export default function Profile() {
+  const auth = getAuth();
+  const [user, loading] = useAuthState(auth);
+  console.log(user)
 
-export default Profile;
+  if (loading) {
+    return <Loading />;
+  }
+  
+  return (
+    <Fragment>
+      <main className="pl-10">
+        {/* Settings forms */}
+        <div className="divide-y divide-white/5">
+          <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
+            <div>
+              <h2 className="text-base font-semibold leading-7 text-white">
+                Personal Information
+              </h2>
+              <p className="mt-1 text-sm leading-6 text-gray-400">
+                Update as many times as you need.
+              </p>
+            </div>
+
+            <form className="md:col-span-2">
+              <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+                <div className="col-span-full flex items-center gap-x-8">
+                  <Image
+                    src={user.photoURL}
+                    alt="Profile Picture"
+                    width={1000}
+                    height={1000}
+                    className="h-24 w-24 flex-none rounded-lg bg-gray-800 object-cover"
+                  />
+                  <div>
+                    <input
+                      id="user-photo"
+                      name="user-photo"
+                      type="file"
+                      hidden={true}
+                    />
+                    <label
+                      type="button"
+                      for="user-photo"
+                      className="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20"
+                    >
+                      Change Avatar
+                    </label>
+                    <span id="file-chosen" className="px-2 text-white">
+                      No file chosen
+                    </span>
+                  </div>
+                </div>
+
+                <div className="col-span-full">
+                  <label
+                    htmlFor="username"
+                    className="block text-sm font-medium leading-6 text-white"
+                  >
+                    Username
+                  </label>
+                  <div className="mt-2">
+                    <div className="pl-3 flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                      <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        autoComplete="username"
+                        className="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
+                        defaultValue={user.displayName}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-span-full">
+                  <label
+                    htmlFor="currency"
+                    className="block text-sm font-medium leading-6 text-white"
+                  >
+                    Preferred Currency
+                  </label>
+                  <div className="mt-2">
+                    <select
+                      id="timezone"
+                      name="timezone"
+                      className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
+                    >
+                      <option value="SGD">Singapore Dollar</option>
+                      <option value="MYR">Malaysian Ringgit</option>
+                      <option value="USD">United States Dollar</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-8 flex">
+                <button
+                  type="submit"
+                  className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+
+          
+        </div>
+      </main>
+    </Fragment>
+  );
+}
