8f1d065afa99b6f2430a239dc3887ceb1eaf869d
L1MJH <lolflgc@icloud.com>
2023-06-19 13:38:38 +0800

    Changed loading icon to make it visible in table

--- pages/loading.jsx
+++ pages/loading.jsx
@@ -4,10 +4,10 @@ const Loading = () => {
   return (
     <div className="grid h-screen place-items-center">
       <div
-        class="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] text-slate-50"
+        class="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] text-blue-400"
         role="status"
       >
-        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] text-slate-50">
+        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
           Loading...
         </span>
       </div>
