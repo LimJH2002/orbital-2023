313a4b1e77eb6f9fe0589ac928603a586454e797
L1MJH <lolflgc@icloud.com>
2023-06-21 12:53:50 +0800

    News page now shows news from Yahoo Finance

--- data/news-posts.jsx
+++ data/news-posts.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const posts = [
   {
     id: 1,
@@ -19,7 +21,7 @@ export const posts = [
     },
   },
   {
-    id: 1,
+    id: 2,
     title: "Boost your conversion rate",
     href: "#",
     description:
@@ -38,7 +40,26 @@ export const posts = [
     },
   },
   {
-    id: 1,
+    id: 3,
+    title: "Boost your conversion rate",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Marketing", href: "#" },
+    author: {
+      name: "Michael Foster",
+      role: "Co-Founder / CTO",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 4,
     title: "Boost your conversion rate",
     href: "#",
     description:
@@ -57,3 +78,41 @@ export const posts = [
     },
   },
 ];
+
+export function Posts({ value }) {
+  return <div>{console.log(value)}</div>;
+}
+
+const options = {
+  method: "POST",
+  url: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list",
+  params: { snippetCount: "28" },
+  headers: {
+    "content-type": "text/plain",
+    "X-RapidAPI-Key": "93909041a2msh87572216c7286b8p1c54b7jsna41785edf865",
+    "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+  },
+  data: "Pass in the value of uuids field returned right in this endpoint to load the next page, or leave empty to load first page",
+};
+
+let ActualPosts = undefined;
+
+try {
+  const response = await axios.request(options);
+  console.log(response.data.data.main.stream);
+  ActualPosts = response.data.data.main.stream;
+} catch (error) {
+  console.error(error);
+}
+
+export const Res = ActualPosts.map((news) => ({
+  id: news.id,
+  title: news.content.title,
+  date: news.content.pubDate,
+  datetime: news.content.pubDate,
+  href: news.content.clickThroughUrl,
+  imageUrl: news.content.thumbnail.resolutions[0].url,
+  author: {
+    name: news.content.provider.displayName,
+  },
+}));
--- pages/news.jsx
+++ pages/news.jsx
@@ -1,6 +1,9 @@
 import { posts } from "@/data/news-posts";
+import { Posts } from "@/data/news-posts";
+import { Res } from "@/data/news-posts";
+import dynamic from "next/dynamic";
 
-export default function Example() {
+function News() {
   return (
     <div className="py-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -12,16 +15,18 @@ export default function Example() {
             Keep in touch with the latest news.
           </p>
         </div>
+
+        {/* <Posts /> */}
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
+          {Res.map((post) => (
             <article
               key={post.id}
-              className="flex flex-col items-start justify-between"
+              className="flex flex-col items-start"
             >
               <div className="relative w-full">
                 <img
                   src={post.imageUrl}
-                  alt=""
+                  alt="Thumbnail"
                   className="aspect-[16/9] w-full rounded-2xl bg-gray-300 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
                 />
                 <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
@@ -29,14 +34,14 @@ export default function Example() {
               <div className="max-w-xl">
                 <div className="mt-8 flex items-center gap-x-4 text-xs">
                   <time dateTime={post.datetime} className="text-gray-300">
-                    {post.date}
+                    {post.date.substring(0, 10)}
                   </time>
-                  <a
+                  {/* <a
                     href={post.category.href}
                     className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                   >
                     {post.category.title}
-                  </a>
+                  </a> */}
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-300 group-hover:text-gray-500">
@@ -45,9 +50,9 @@ export default function Example() {
                       {post.title}
                     </a>
                   </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-300">
+                  {/* <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-300">
                     {post.description}
-                  </p>
+                  </p> */}
                 </div>
               </div>
             </article>
@@ -57,3 +62,5 @@ export default function Example() {
     </div>
   );
 }
+
+export default dynamic (() => Promise.resolve(News), {ssr: false})
