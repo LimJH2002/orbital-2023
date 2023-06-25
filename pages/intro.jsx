b233c43801e89aac3249fe47401e6a7fbe00e645
L1MJH <lolflgc@icloud.com>
2023-06-25 22:54:58 +0800

    Added intro2 page

--- pages/intro.jsx
+++ pages/intro.jsx
@@ -57,6 +57,7 @@ export default function Intro() {
               name="username"
               id="username"
               autoComplete="username"
+              required="true"
               placeholder="username"
               className="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
             />
--- pages/intro2.jsx
+++ pages/intro2.jsx
@@ -1,78 +1,92 @@
 import { useRouter } from "next/router";
 import { motion } from "framer-motion";
-import {
-  DocumentIcon,
-  PresentationChartBarIcon,
-} from "@heroicons/react/24/outline";
 
 const STAGGER_CHILD_VARIANTS = {
   hidden: { opacity: 0, y: 20 },
   show: { opacity: 1, y: 0, transition: { duration: 0.4, type: "spring" } },
 };
 
-export default function InitSettings() {
+export default function Intro2() {
   const router = useRouter();
+
   return (
     <motion.div
-      className="z-10 mx-5 flex flex-col items-center space-y-10 text-center sm:mx-auto"
-      variants={{
-        hidden: { opacity: 0, scale: 0.95 },
-        show: {
-          opacity: 1,
-          scale: 1,
-          transition: {
-            staggerChildren: 0.2,
-          },
-        },
-      }}
-      initial="hidden"
-      animate="show"
-      exit="hidden"
-      transition={{ duration: 0.3, type: "spring" }}
+      className="z-10 grid place-items-center h-screen"
+      exit={{ opacity: 0, scale: 0.95 }}
+      transition={{ duration: 0.5, type: "spring" }}
     >
       <motion.div
-        variants={STAGGER_CHILD_VARIANTS}
-        className="flex flex-col items-center space-y-10 text-center"
+        variants={{
+          show: {
+            transition: {
+              staggerChildren: 0.5,
+            },
+          },
+        }}
+        initial="hidden"
+        animate="show"
+        className="mx-5 flex flex-col items-center space-y-10 text-center sm:mx-auto"
       >
-        <p className="text-2xl font-bold tracking-tighter text-white">
-          Papermark
-        </p>
-        <h1 className="font-display max-w-md text-3xl font-semibold text-gray-100 transition-colors sm:text-4xl">
-          Which document do you want to share today?
-        </h1>
-      </motion.div>
-      <motion.div
-        variants={STAGGER_CHILD_VARIANTS}
-        className="grid w-full grid-cols-1 divide-y divide-gray-900 text-white rounded-md border border-gray-800 md:grid-cols-2 md:divide-x"
-      >
-        <button
+        <motion.h1
+          className="font-display text-4xl font-bold text-white transition-colors sm:text-5xl"
+          variants={STAGGER_CHILD_VARIANTS}
+        >
+          Hi {"username"}, <br></br> nice to meet you!
+        </motion.h1>
+        <motion.p
+          className="max-w-md text-gray-200 transition-colors sm:text-lg"
+          variants={STAGGER_CHILD_VARIANTS}
+        >
+          Just a few more questions...
+        </motion.p>
+
+        <motion.div
+          className="max-w-md text-gray-200 transition-colors sm:text-lg"
+          variants={STAGGER_CHILD_VARIANTS}
+        >
+          <motion.p
+            className="max-w-md text-gray-200 transition-colors sm:text-lg mb-1"
+            variants={STAGGER_CHILD_VARIANTS}
+          >
+            How much do you plan to spend per month?
+          </motion.p>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <div className="max-w-lg flex rounded-md shadow-sm">
+              <div className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 text-white sm:text-sm">
+                <select
+                  id="currency"
+                  name="currency"
+                  className="focus:ring-gray-300 focus:border-gray-300 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-white sm:text-sm rounded-md"
+                >
+                  <option>SGD</option>
+                  <option>MYR</option>
+                  <option>USD</option>
+                </select>
+              </div>
+              <input
+                required
+                type="number"
+                min="0.01"
+                name="amount"
+                step=".01"
+                placeholder="Monthly Budget"
+                id="amount"
+                className="flex-1 block w-full focus:ring-gray-300 focus:border-gray-300 min-w-0 rounded-none rounded-r-md sm:text-sm bg-transparent border-gray-300"
+              />
+            </div>
+          </div>
+        </motion.div>
+        <motion.button
+          variants={STAGGER_CHILD_VARIANTS}
+          className="rounded  px-10 py-2 font-medium transition-colors text-gray-900 bg-gray-100 hover:text-gray-100 hover:bg-gray-500"
           onClick={() =>
             router.push({
-              pathname: "/welcome",
-              query: {
-                type: "pitchdeck",
-              },
+              pathname: "/intro2",
             })
           }
-          className="flex flex-col items-center justify-center overflow-hidden p-5 space-y-5 transition-colors hover:bg-gray-800 md:p-10 min-h-[200px]"
         >
-          <PresentationChartBarIcon className="h-auto  pointer-events-none w-12 sm:w-12" />
-          <p>Pitchdeck</p>
-        </button>
-        <button
-          onClick={() =>
-            router.push({
-              pathname: "/welcome",
-              query: {
-                type: "document",
-              },
-            })
-          }
-          className="flex flex-col items-center justify-center overflow-hidden p-5 space-y-5 transition-colors hover:bg-gray-800 md:p-10 min-h-[200px]"
-        >
-          <DocumentIcon className="h-auto  pointer-events-none w-12 sm:w-12" />
-          <p>Another document</p>
-        </button>
+          Start My Journey
+        </motion.button>
       </motion.div>
     </motion.div>
   );
