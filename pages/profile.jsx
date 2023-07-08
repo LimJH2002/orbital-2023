2f184f788512cc010d4f86745ef4b3552a16b85e
L1MJH <lolflgc@icloud.com>
2023-07-08 23:32:07 +0800

    Update profile.jsx

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -228,7 +228,7 @@ export default function Profile() {
               <div className="mt-8 flex">
                 <button
                   type="submit"
-                  onClick={()=>router.reload()}
+                  onClick={() => router.reload()}
                   className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                 >
                   Save
