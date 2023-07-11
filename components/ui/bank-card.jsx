05f083d1dcb32fa60ca720567e29a7de08be91e6
L1MJH <lolflgc@icloud.com>
2023-07-11 14:09:50 +0800

    Added link bank components

--- components/ui/bank-card.jsx
+++ components/ui/bank-card.jsx
@@ -9,16 +9,16 @@ import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
 
 export default function BankCard(props) {
   return (
-    <Card className="flex-row mx-20">
+    <Card className="flex-row mx-20 mb-5">
       <CardHeader
         shadow={false}
         floated={false}
-        className="w-2/5 shrink-0 m-0 rounded-r-none"
+        className="w-1/4 shrink-0 m-0 rounded-r-none"
       >
         <img
-          src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
-          alt="image"
-          className="w-full h-full object-cover"
+          src={props.img}
+          alt={props.name + " logo"}
+          className="h-auto scale-75"
         />
       </CardHeader>
       <CardBody>
@@ -26,16 +26,17 @@ export default function BankCard(props) {
           {props.name}
         </Typography>
         <Typography color="gray" className="font-normal mb-8">
-          Like so many organizations these days, Autodesk is a company in
-          transition. It was until recently a traditional boxed software company
-          selling licenses. Yet its own business model disruption is only part
-          of the story
+          Supported Functionality:
+          <br />
+          View Transactions, Label Transactions.
         </Typography>
         <a href="#" className="inline-block">
-          <Button variant="text" className="flex items-center gap-2">
-            Learn More
-            <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
-          </Button>
+          <button
+            type="button"
+            className="rounded-full bg-blue-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Link Bank
+          </button>
         </a>
       </CardBody>
     </Card>
