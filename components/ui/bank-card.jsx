c9ae8248d486d51451df4283e3ab42ae333db309
L1MJH <lolflgc@icloud.com>
2023-07-16 22:33:58 +0800

    Changed card component codebase

--- components/ui/bank-card.jsx
+++ components/ui/bank-card.jsx
@@ -10,37 +10,31 @@ export default function BankCard(props) {
   const [Link, setLink] = useState(false);
 
   return (
-    <Card className="flex-row mx-20 mb-5 rounded-2xl">
-      <CardHeader
-        shadow={false}
-        floated={false}
-        className="w-2/5 shrink-0 m-0 rounded-r-none"
-      >
-        <img
-          src={props.img}
-          alt={props.name + " logo"}
-          className="scale-75 pt-10"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h4" color="blue-gray" className="mb-2">
+    <div class="p-5 flex flex-col mx-20 mb-5 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <img
+        class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-96 m-9"
+        src={props.img}
+        alt={props.name + " logo"}
+      />
+      <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {props.name}
-        </Typography>
-        <Typography color="gray" className="font-normal mb-8">
+        </h5>
+        <p class="mb-6 font-normal text-gray-700 dark:text-gray-400">
           Supported Functionality:
           <br />
           View Transactions, Label Transactions.
-        </Typography>
+        </p>
         <a href={props.redirect} target="_blank" className="inline-block">
           <button
             type="button"
-            onClick={()=>setLink(prev => !prev)}
+            onClick={() => setLink((prev) => !prev)}
             className="rounded-full bg-blue-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             {Link ? "Unlink" : "Link Bank"}
           </button>
         </a>
-      </CardBody>
-    </Card>
+      </div>
+    </div>
   );
 }
