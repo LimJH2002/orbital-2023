44ec50ffb00a8183f2d2a2c7b37d1763afac762c
L1MJH <lolflgc@icloud.com>
2023-07-16 23:10:19 +0800

    Refined hover animation

--- components/bank-tab.jsx
+++ components/bank-tab.jsx
@@ -35,14 +35,13 @@ export default function BankTab(props) {
           {tabs.map((tab, tabIdx) => (
             <a
               key={tab.name}
-              href={tab.href}
               className={classNames(
                 tab.current
                   ? "text-gray-900"
                   : "text-gray-500 hover:text-gray-700",
                 tabIdx === 0 ? "rounded-l-lg" : "",
                 tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
-                "group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+                "cursor-pointer group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-100 focus:z-10"
               )}
               aria-current={tab.current ? "page" : undefined}
               onClick={() => props.setFunc(tab.id)}
--- pages/bank/overview.jsx
+++ pages/bank/overview.jsx
@@ -22,10 +22,10 @@ const Overview = (props) => {
       <button
         type="button"
         onClick={() => props.setFunc("2")}
-        className="w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="w-full text-white border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
-        <TbCashBanknoteOff className="mx-auto h-12 w-12 text-white" />
-        <span className="mt-2 block text-sm font-medium text-white">
+        <TbCashBanknoteOff className="mx-auto h-12 w-12" />
+        <span className="mt-2 block text-sm font-medium">
           Link Your First Bank Account!
         </span>
       </button>
