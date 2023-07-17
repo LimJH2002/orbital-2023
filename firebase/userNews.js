4aa19ba58fb67d375cf341b481f861c0fc8c3656
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-07-17 17:59:27 +0800

    Add addNews API

--- firebase/userNews.js
+++ firebase/userNews.js
@@ -1,7 +1,6 @@
 import { db } from "@/firebase/firebaseApp";
 import { useAuth } from "@/context/AuthContext";
-import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
-import { month } from "@/data/month-year";
+import { doc, getDoc, arrayUnion, updateDoc } from "firebase/firestore";
 
 // GET: http://localhost:3000/api/dashboard/1
 export async function getSavedNews(req, res) {
@@ -14,9 +13,45 @@ export async function getSavedNews(req, res) {
                 savedNews:[]
             });
         }
+        if (!getSnap.data().newsCount) {
+            await updateDoc(docRef, {
+                newsCount:0
+            });
+        }
         const docSnap = await getDoc(docRef);
         console.log("news: ", docSnap.data().savedNews);
         res.status(200).json(docSnap.data().savedNews);
+    } catch (err) {
+      console.log(err);
+      res.status(404).json({ error: "Error while fetching data" });
+    }
+}
+
+export async function addNews(req, res) {
+    const { userId } = req.query;
+      try {
+        const article = req.body;
+        const docRef = doc(db, 'users', userId);
+        const getSnap = await getDoc(docRef);
+        if (!getSnap.data().savedNews) {
+            await updateDoc(docRef, {
+                savedNews:[]
+            });
+        }
+        if (!getSnap.data().newsCount) {
+            await updateDoc(docRef, {
+                newsCount:0
+            });
+        }
+        const docSnap = await getDoc(docRef);
+        const n = docSnap.data().newsCount;
+        await updateDoc(docRef, {
+            savedNews: arrayUnion({...article, "id": n})
+        })
+        await updateDoc(docRef, {newsCount:n+1});
+        console.log("news: ", docSnap.data().savedNews);
+        const newSnap = await getDoc(docRef);
+        res.status(200).json(newSnap.data().savedNews);
     } catch (err) {
       console.log(err);
       res.status(404).json({ error: "Error while fetching data" });
