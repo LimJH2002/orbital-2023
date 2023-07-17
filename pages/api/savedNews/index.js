454017ae00770ced15ecaeae7d7aa93889a45f9b
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-07-17 21:43:37 +0800

    Add delete news API

--- pages/api/savedNews/index.js
+++ pages/api/savedNews/index.js
@@ -1,4 +1,4 @@
-import { addNews, getSavedNews } from "@/firebase/userNews";
+import { addNews, getSavedNews, deleteSavedNews } from "@/firebase/userNews";
 
 export default async function handler(req, res) {
     const { method } = req;
@@ -10,8 +10,11 @@ export default async function handler(req, res) {
         case 'POST':
             addNews(req, res);
             break;
+        case 'PUT':
+            deleteSavedNews(req, res);
+            break;
         default : 
-            res.setHeader('Allow', ['POST']);
+            res.setHeader('Allow', ['GET', 'POST', 'PUT']);
             res.status(405).end(`Method ${method} Not Allowed`);
             break;
     }
