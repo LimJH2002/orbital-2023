fa9e3143bf530cf2e3ad9c07300ec30038582e37
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-07-18 01:45:07 +0800

    Added username in Dashboard

--- components/dashboard.jsx
+++ components/dashboard.jsx
@@ -3,8 +3,37 @@ import Table from "./table";
 import Greetings from "@/functions/Greetings";
 import { useState, useEffect } from "react";
 import Loading from "@/pages/loading";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Dashboard(props) {
+  const { currentUser } = useAuth();
+  const [name, setName] = useState("");
+  const [uid, setUid] = useState("");
+  console.log("uuid", uid)
+
+  useEffect(() => {
+    if (currentUser) {
+      setUid(currentUser.uid);
+    }
+  }, [currentUser]);
+
+  const getUsername = async () => {
+    const response = await fetch("/api/user?userId=" + uid, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((e) => e.json());
+    setName(response.username)
+  }
+
+  useEffect(() => {
+    if(uid) {
+      getUsername();
+    }
+  },
+  [uid])
+
 
   return (
     <div className="min-h-full">
@@ -12,7 +41,7 @@ export default function Dashboard(props) {
         <header className="py-10">
           <div className="flex flex-row max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold text-white">
-              {Greetings()}, <br /> {"Username"}
+              {Greetings()}, <br /> { name }
             </h1>
             <div className="w-48 ml-auto flex items-center">
               <button
