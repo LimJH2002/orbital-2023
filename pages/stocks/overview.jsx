20936274c040a9653f1553f24ea48f3f354752bd
L1MJH <lolflgc@icloud.com>
2023-07-21 20:58:31 +0800

    Added card

--- pages/stocks/overview.jsx
+++ pages/stocks/overview.jsx
@@ -81,47 +81,50 @@ export default function MarketOverviewWidget() {
         }}
       />
 
-      <form className="ml-10">
-        <label
-          htmlFor="phone-number"
-          className="block text-sm font-medium leading-6 text-white"
-        >
-          Add Stocks
-        </label>
-        <div className="flex ">
-          <div className="relative mt-2 rounded-md shadow-sm">
-            <div className="absolute inset-y-0 left-0 flex items-center">
-              <label htmlFor="country" className="sr-only">
-                Symbols
-              </label>
-              <select
-                id="exchange"
-                name="exchange"
-                autoComplete="exchange"
-                className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-              >
-                <option>NASDAQ</option>
-                <option>NYSE</option>
-              </select>
-            </div>
-            <input
-              type="text"
-              name="symbol"
-              id="symbol"
-              className="block w-full rounded-md border-0 pl-36 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Enter Stock Symbol"
-            />
-          </div>
-          <button
-            onClick={() =>
-              setSymbols((prev) => [...prev, { s: "NASDAQ:AMZN" }])
-            }
-            className="rounded-md bg-indigo-600 px-5 mx-3 mt-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      <div className="ml-10 bg-gray-800 rounded-xl p-3">
+        <form>
+          <label
+            htmlFor="phone-number"
+            className="block text-sm font-medium leading-6 text-white"
           >
-            Add Stock
-          </button>
-        </div>
-      </form>
+            Add Stocks
+          </label>
+          <div className="flex">
+            <div className="relative mt-2 rounded-md shadow-sm">
+              <div className="absolute inset-y-0 left-0 flex items-center">
+                <label htmlFor="country" className="sr-only">
+                  Symbols
+                </label>
+                <select
+                  id="exchange"
+                  name="exchange"
+                  autoComplete="exchange"
+                  className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                >
+                  <option>NASDAQ</option>
+                  <option>NYSE</option>
+                </select>
+              </div>
+              <input
+                type="text"
+                name="symbol"
+                id="symbol"
+                className="block w-full rounded-md border-0 pl-36 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Stock Symbol"
+              />
+            </div>
+            <button
+              onClick={() =>
+                setSymbols((prev) => [...prev, { s: "NASDAQ:AMZN" }])
+              }
+              className="rounded-md bg-indigo-600 px-5 mx-3 mt-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Add Stock
+            </button>
+          </div>
+        </form>
+        <div>Hiiii</div>
+      </div>
     </div>
   );
 }
