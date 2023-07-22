aafe22847dc1df49c9ee12d852da4ba1500020c4
L1MJH <lolflgc@icloud.com>
2023-07-22 22:00:28 +0800

    Added search stocks functionality

---
+++ components/ui/search.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useState } from "react";
+import { NASDAQ } from "@/data/nasdaq";
+
+const Searchbar = () => {
+  const [activeSearch, setActiveSearch] = useState([]);
+
+  const handleSearch = (e) => {
+    const upperCaseValue = e.target.value.toUpperCase();
+
+    if (upperCaseValue == "") {
+      setActiveSearch([]);
+      return false;
+    }
+    setActiveSearch(NASDAQ.filter((w) => w.includes(upperCaseValue)));
+  };
+
+  return (
+    <form className="w-96 relative">
+      <div className="relative">
+        <input
+          type="search"
+          placeholder="Search Symbols"
+          className="w-full p-4 rounded-xl bg-slate-800 text-white"
+          onChange={(e) => handleSearch(e)}
+        />
+      </div>
+
+      {activeSearch.length > 0 && (
+        <div className="absolute top-20 p-4 bg-slate-800 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col overflow-y-auto max-h-60 z-10">
+          {activeSearch.map((s) => (
+            <span className="hover:bg-slate-700 p-2 rounded-xl">{s}</span>
+          ))}
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default Searchbar;
