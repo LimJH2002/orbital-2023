526c9f363753798af4ef4d04d38fb6f67c6229fa
L1MJH <lolflgc@icloud.com>
2023-07-24 01:04:40 +0800

    Update transaction-item.jsx

--- components/transaction-item.jsx
+++ components/transaction-item.jsx
@@ -12,9 +12,8 @@ function TransactionItem({
   bank,
   selectedOpt,
   exclude,
-  setExclude
+  setExclude,
 }) {
-
   return (
     <tr
       key={transaction.title}
@@ -23,7 +22,12 @@ function TransactionItem({
       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
         {transaction.title}
       </td>
-      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+      <td
+        className={
+          "whitespace-nowrap px-3 py-4 text-sm " +
+          (transaction.type === "Money-in" ? "text-lime-500" : "text-red-600")
+        }
+      >
         {transaction.type}
       </td>
       <td className=" px-3 py-4 text-sm">{label(transaction.category)}</td>
